5e6f7bb7c5bda98b4ab8a611357d84c08fc1683c
Hennadii Borysevych <genuch3@gmail.com>
2024-11-05 18:26:36 +0100

    added VacanciesOrm and VacanciesRepliesOrm models

--- src/database.py
+++ src/database.py
@@ -1,46 +1,42 @@
+import asyncio
 from typing import Annotated
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession,  async_sessionmaker
-from sqlalchemy.orm import Session, sessionmaker, DeclarativeBase
-from sqlalchemy import create_engine, text,  MetaData, URL
+
+from sqlalchemy import String, create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+
 from config import settings
 
-engine = create_engine(
+sync_engine = create_engine(
     url=settings.DATABASE_URL_psycopg2,
     echo=True,
-    pool_size=5,
-    max_overflow=10,
-    # pool_recycle=30,
-    # pool_pre_ping=True,
-    # pool_use_lifo=True
+    # pool_size=5,
+    # max_overflow=10,
 )
 
 async_engine = create_async_engine(
     url=settings.DATABASE_URL_asyncpg,
     echo=True,
-    pool_size=5,
-    max_overflow=10,
-    # pool_recycle=30,
-    # pool_pre_ping=True,
-    # pool_use_lifo=True
 )
 
-# with engine.connect() as conn:
-#     result = conn.execute(text("SELECT version()"))
-#     print(f"result: {result=}")
+session_factory = sessionmaker(sync_engine)
+async_session_factory = async_sessionmaker(async_engine)
 
-
-async def get_async_session() -> AsyncSession:
-    async with engine.connect() as conn:
-        result = await conn.execute(text("SELECT version()"))
-        print(f"result: {result=}")
-
-
-session = sessionmaker(
-    engine,
-    expire_on_commit=False
-)
-
-str_200 = Annotated[str, 200]
+str_256 = Annotated[str, 256]
 
 class Base(DeclarativeBase):
-    pass
+    type_annotation_map = {
+        str_256: String(256)
+    }
+
+    repr_cols_num = 3
+    repr_cols = tuple()
+    
+    def __repr__(self):
+        """Relationships не используются в repr(), т.к. могут вести к неожиданным подгрузкам"""
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
--- src/models.py
+++ src/models.py
@@ -1,47 +1,166 @@
+
 import datetime
-from typing import Annotated
-from sqlalchemy import  Table, Column, Integer, String, MetaData, text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
 import enum
-from database import Base
+from typing import Annotated, Optional
 
-# metadata = MetaData()
+from sqlalchemy import (
+    TIMESTAMP,
+    CheckConstraint,
+    Column,
+    Enum,
+    ForeignKey,
+    Index,
+    Integer,
+    MetaData,
+    PrimaryKeyConstraint,
+    String,
+    Table,
+    text,
+)
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-# users = Table('users', metadata,
-#     Column('id', Integer, primary_key=True),
-#     Column('name', String),
-#     Column('fullname', String),
-# )
+from database import Base, str_256
 
-id_primary = Annotated[int, mapped_column(primary_key=True)]
-created_at = Annotated[datetime.datetime, mapped_column(
-        server_default=text("TIMEZONE('utc', now())")
-    )]
+intpk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
 updated_at = Annotated[datetime.datetime, mapped_column(
         server_default=text("TIMEZONE('utc', now())"),
-        onupdate=datetime.datetime.now,
+        onupdate=datetime.datetime.utcnow,
     )]
 
 
-class User(Base):
-    __tablename__ = 'user'
+class WorkersOrm(Base):
+    __tablename__ = "workers"
 
-    id: Mapped[id_primary]
-    name: Mapped[str]
+    id: Mapped[intpk]
+    username: Mapped[str]
 
+    resumes: Mapped[list["ResumesOrm"]] = relationship(
+        back_populates="worker",
+    )
+
+    resumes_parttime: Mapped[list["ResumesOrm"]] = relationship(
+        back_populates="worker",
+        primaryjoin="and_(WorkersOrm.id == ResumesOrm.worker_id, ResumesOrm.workload == 'parttime')",
+        order_by="ResumesOrm.id.desc()",
+    )
+    
 
 class Workload(enum.Enum):
-    part_time = "part_time"
-    full_time = "full_time"
+    parttime = "parttime"
+    fulltime = "fulltime"
 
 
-class Resume(Base):
-    __tablename__ = 'resume'
+class ResumesOrm(Base):
+    __tablename__ = "resumes"
 
-    id: Mapped[id_primary]
-    title: Mapped[str]
-    compensation: Mapped[int | None]
+    id: Mapped[intpk]
+    title: Mapped[str_256]
+    compensation: Mapped[Optional[int]]
     workload: Mapped[Workload]
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
-    created_at: Mapped[created_at] 
+    worker_id: Mapped[int] = mapped_column(ForeignKey("workers.id", ondelete="CASCADE"))
+    created_at: Mapped[created_at]
     updated_at: Mapped[updated_at]
+
+    worker: Mapped["WorkersOrm"] = relationship(
+        back_populates="resumes",
+    )
+
+    vacancies_replied: Mapped[list["VacanciesOrm"]] = relationship(
+        back_populates="resumes_replied",
+        secondary="vacancies_replies",
+    )
+
+    repr_cols_num = 2
+    repr_cols = ("created_at", )
+
+    __table_args__ = (
+        Index("title_index", "title"),
+        CheckConstraint("compensation > 0", name="checl_compensation_positive"),
+    )
+
+
+class VacanciesOrm(Base):
+    __tablename__ = "vacancies"
+
+    id: Mapped[intpk]
+    title: Mapped[str_256]
+    compensation: Mapped[Optional[int]]
+
+    resumes_replied: Mapped[list["ResumesOrm"]] = relationship(
+        back_populates="vacancies_replied",
+        secondary="vacancies_replies",
+    )
+
+
+class VacanciesRepliesOrm(Base):
+    __tablename__ = "vacancies_replies"
+
+    resume_id: Mapped[int] = mapped_column(
+        ForeignKey("resumes.id", ondelete="CASCADE"),
+        primary_key=True,
+    )
+    vacancy_id: Mapped[int] = mapped_column(
+        ForeignKey("vacancies.id", ondelete="CASCADE"),
+        primary_key=True,
+    )
+
+    cover_letter: Mapped[Optional[str]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+metadata_obj = MetaData()
+
+workers_table = Table(
+    "workers",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("username", String),
+)
+
+resumes_table = Table(
+    "resumes",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(256)),
+    Column("compensation", Integer, nullable=True),
+    Column("workload", Enum(Workload)),
+    Column("worker_id", ForeignKey("workers.id", ondelete="CASCADE")),
+    Column("created_at", TIMESTAMP,server_default=text("TIMEZONE('utc', now())")),
+    Column("updated_at", TIMESTAMP,server_default=text("TIMEZONE('utc', now())"), onupdate=datetime.datetime.utcnow),
+)
+
+vacancies_table = Table(
+    "vacancies",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("title", String),
+    Column("compensation", Integer, nullable=True),
+)
+
+vacancies_replies_table = Table(
+    "vacancies_replies",
+    metadata_obj,
+    Column("resume_id", ForeignKey("resumes.id", ondelete="CASCADE"), primary_key=True),
+    Column("vacancy_id", ForeignKey("vacancies.id", ondelete="CASCADE"), primary_key=True),
+)
